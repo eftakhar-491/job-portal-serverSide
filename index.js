2ae2bf5328a199cb74b01bc4de13c09f678c247c
eftakhar-491 <eftt042@gmail.com>
2024-12-13 00:19:30 +0600

    server all route create done

--- index.js
+++ index.js
@@ -19,44 +19,109 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // await client.connect();
-
     const database = client.db("jobPortalDB");
     const jobs = database.collection("jobs");
-
+    const users = database.collection("users");
+    const apply = database.collection("apply");
+    // index route
     app.get("/", async (req, res) => {
-      //   const result = await users.find().toArray();
       res.send({ process: "success" });
     });
+
+    // jobs route (sobai dekte parbo)
     app.get("/jobs", async (req, res) => {
       const result = await jobs.find().toArray();
       res.send(result);
     });
-
     app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
       const result = await jobs.findOne({ _id: new ObjectId(id) });
       res.send(result);
     });
+
+    // apply route
+    app.post("/apply", async (req, res) => {
+      const userData = req.body;
+      const result = await apply.insertOne(userData);
+      res.send(result);
+    });
+    app.get("/apply/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await apply.find({ worker_email: email }).toArray();
+      res.send(result);
+    });
+    app.patch("/apply/:id", async (req, res) => {
+      const userData = req.body;
+      const id = req.params.id;
+      const result = await apply.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: userData }
+      );
+      res.send(result);
+    });
+    app.delete("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await users.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    // recruters route
     app.post("/jobs", async (req, res) => {
       const userData = req.body;
       const result = await jobs.insertOne(userData);
       res.send(result);
     });
-    // app.patch("/tourists/:id", async (req, res) => {
-    //   const userData = req.body;
-    //   const id = req.params.id;
-    //   const result = await users.updateOne(
-    //     { _id: new ObjectId(id) },
-    //     { $set: userData }
-    //   );
-    //   res.send(result);
-    // });
-    // app.delete("/tourists/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const result = await users.deleteOne({ _id: new ObjectId(id) });
-    //   res.send(result);
-    // });
+    app.get("/jobs/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await jobs.find({ hr_email: email }).toArray();
+      res.send(result);
+    });
+
+    app.patch("/jobs/:id", async (req, res) => {
+      const userData = req.body;
+      const id = req.params.id;
+      const result = await users.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: userData }
+      );
+      res.send(result);
+    });
+    app.delete("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await users.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    //users
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await users.findOne({ uid: id }).toArray();
+      res.send(result);
+    });
+    app.post("/users", async (req, res) => {
+      const userData = req.body;
+      const result = await users.insertOne(userData);
+      res.send(result);
+    });
+
+    app.patch("/users/:email", async (req, res) => {
+      const userData = req.body;
+      const email = req.params.email;
+      // sommossa ase
+      const result = await users.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: userData }
+      );
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await users.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    //--------------------
 
     await client.db("admin").command({ ping: 1 });
     // console.log(
